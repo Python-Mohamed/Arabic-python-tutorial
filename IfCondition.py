17b3ea334d1b6cff66af5aac1dad032879971482
Python-Mohamed <167488746+Python-Mohamed@users.noreply.github.com>
2024-04-19 03:14:55 +0200

    Add files via upload

---
+++ IfCondition.py
@@ -0,0 +1,39 @@
+# leapYear = int(input("Enter the year you want to check: "))
+# if leapYear % 4 == 0:
+#     if leapYear % 100 == 0:
+#         if leapYear % 400 == 0:
+#             print("Leap Year")
+#         else:
+#             print("Not Leap Year")
+#     else:
+#         print("Leap Year")
+# else:
+#     print("Not Leap Year")
+
+# if (leapYear % 4 == 0) and (leapYear % 100 != 0 or leapYear % 400 == 0):
+#     print(leapYear, "is a leap year")
+# else:
+#     print(leapYear, "is not a leap year")
+
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# number = int(input("Enter a number: "))
+# if number > 0:
+#     print(number, "is a positive number")
+# elif number < 0:
+#     print(number, "is a negative number")
+# else:
+#     print(number, "is a zero number")
+
+# number = int(input("Enter a number: "))
+# if number < 0:
+#     print("Sorry, the number is negative")
+# else:
+#     if number > 0:
+#         print("The number is positive")
+#     else:
+#         print("The number is zero")
